9452f07e5a345413b156fa2701422bc2e4cc7a65
Okonkwo Emmanuel <jahsminemma@gmail.com>
2021-04-14 23:52:14 +0100

    progress

--- Atm_Operation.py
+++ Atm_Operation.py
@@ -1,12 +1,14 @@
 
 import random
-Database = {}
+Database = { 8705029962: ["emma"," bola","bola@gmail.com", 1234, 0]}
 
 ##INITIALIZE THE SYSTEM
 def init():
     validOption = False
     while validOption == False:
-        print("Welcome to Zuri Banking")
+        print("============================================")
+        print("============Welcome to ZuriBank============")
+        print("============================================") 
         accountRequest = int(input("Do you have acount with us ? \n 1. YES \n 2. NO \n"))
         if (accountRequest == 1):
             validOption = True
@@ -47,9 +49,10 @@ def register():
     accountEmail = input("Enter your email address ? \n ")
     accountPin = int(input("Enter your pin ? \n"))
     accountNumber = generateAccountNumber()
+    currentBalance = 0
 
     #Adding user name, pin and account number to Database
-    Database[accountNumber] = [firstName, lastName, accountEmail, accountPin]
+    Database[accountNumber] = [firstName, lastName, accountEmail, accountPin, currentBalance]
     print("*******Login with your account number and pin******\n************ ensure you keep it save**************")
     getAccountNumber = Database.keys()
     print("************Account Number : %d***********" % accountNumber)
@@ -61,10 +64,33 @@ def register():
 def generateAccountNumber():
     accountNumber = random.randrange(1111111111, 9999999999)
     return accountNumber
+
+
+#get account balance  
+def getBalance(userDetails):
+        return userDetails[4]
+
+#Deposit operation
+def deposit():
+    amount = int(input("How much would you like to deposit \n"))
+    for userDetails in Database.values():
+        userDetails[4] += amount
+        print("Deposit completed successfully")
+        print("Your Balance is %d" % getBalance(userDetails))
+
+
+def withdraw():
+    amount = int(input("How much would you like to withdraw \n"))
+    for userDetails in Database.values():
+        if (amount <= getBalance(userDetails)):
+            userDetails[4] -= amount
+            print("Withdrawal completed succesfully")
+            print("Take your cash")
+        else:
+            print("insuffiecient fund")
     
 
 #run bank operation
-
 def bankOperation():
     print("======You have logged in successfully======")
     for userDetails in Database.values():
@@ -82,20 +108,23 @@ def bankOperation():
         print("=====================================") 
         print("1. Withdraw")
         print("2. Deposit")
-        print("3. Complaints")
+        print("3. Account Balance")
+        print("4. Complaints")
 
         selectedOption = int(input("please select an option \n"))
-        if(selectedOption == 1):
-            amount = int(input("How much would you like to withdraw \n"))
-            print("Take your cash ")
+        if (selectedOption == 1):
+           withdraw()
         elif(selectedOption == 2):
-            depositAmount = int(input("How much  would you like to deposit \n"))
-            print("current Balance is %s" %depositAmount)
-        elif(selectedOption == 3):
-            input("What issue wil you like to report \n")
-            print("Thank you for contacting us")
+            deposit()
+        elif (selectedOption == 3):
+            for userDetails in Database.values():
+                print("Your account balance is %d" % getBalance(userDetails))
+        elif (selectedOption == 4):
+             input("What issue wil you like to report \n")
+             print("Thank you for contacting us")
+            
 
-        inputOption = int(input("DO you want to do any transactions again \n 1. Yes \n 2. NO \n"))
+        inputOption = int(input("Would you like to do another transaction \n 1. Yes \n 2. NO \n"))
         if (inputOption == 1):
              operationRequest = True
         elif (inputOption == 2):
